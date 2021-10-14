d645ba674ebb443f0b898a180559ce2bedde6588
tyanotyrano <tyrano09@gmail.com>
2021-10-14 23:24:24 +0900

    feat(templates package): 기본 화면 구성
    Spring Security 학습에 필요한 권한자별 접속가능한 기본 화면을 구성한다.

---
+++ src/main/java/com/tyranotyrano/springsecurityform/controller/SampleController.java
@@ -0,0 +1,47 @@
+package com.tyranotyrano.springsecurityform.controller;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+	@GetMapping("/")
+	public String index(Model model, Principal principal) {
+		if (principal == null) {
+			// 로그인 안한 사람
+			model.addAttribute("message", "index : Hello Spring Security!!!");
+		}
+		else {
+			// 로그인 한 사람
+			model.addAttribute("message", "index : " + principal.getName());
+		}
+
+
+		return "index";
+	}
+
+	@GetMapping("/info")
+	public String info(Model model) {
+		// Info : 로그인 여부에 상관없이 아무나 접근가능
+		model.addAttribute("message", "info : 아무나 접근 가능");
+		return "info";
+	}
+
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, Principal principal) {
+		// Dashboard : 로그인한 사용자만 접근가능 -> Principal 필요
+		model.addAttribute("message", "dashboard : " + principal.getName());
+		return "dashboard";
+	}
+
+	@GetMapping("/admin")
+	public String admin(Model model, Principal principal) {
+		// admin : 관리자만 접근 가능 -> Principal 필요
+		model.addAttribute("message", "admin : " + principal.getName());
+		return "admin";
+	}
+}
