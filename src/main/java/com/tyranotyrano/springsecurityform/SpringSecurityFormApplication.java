e33f5efed16b1d2304d9be536bda9adb202fe8f6
tyanotyrano <tyrano09@gmail.com>
2021-10-14 23:07:52 +0900

    create : Spring Security 학습 프로젝트 생성

---
+++ src/main/java/com/tyranotyrano/springsecurityform/SpringSecurityFormApplication.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano.springsecurityform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyranotyrano/springsecurityform/SpringSecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyranotyrano.springsecurityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
