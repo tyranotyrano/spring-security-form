73b5c744ff5b2c4ba4812721d176c3662ca9445f
tyanotyrano <tyrano09@gmail.com>
2021-10-15 23:35:34 +0900

    config(SecurityConfig.java): Spring Security 커스텀 계정 추가
    Spring Security 에 커스텀 계정을 추가한다.
    - application.yml : properties 방식으로 admin 계정 추가
    - SecurityConfig : 일반계정(tyrano), 관리자 계정(admin) 2개 추가

--- src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
+++ src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.tyranotyrano.springsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		http.formLogin();
 		http.httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("tyrano").password("{noop}1234").roles("USER")
+			.and()
+			.withUser("admin").password("{noop}1234").roles("ADMIN");
+
 	}
 }
