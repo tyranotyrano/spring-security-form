759cb3b6a5edb5a277b9a69c1172049122634659
tyanotyrano <tyrano09@gmail.com>
2021-10-17 15:18:16 +0900

    test(AccountControllerTest.java): AccountController Web Security 테스트 코드 추가
    Account 별 페이지 접근에 대한 테스트 코드를 추가한다.
    - MockMvc 를 이용하여 Controller 테스트 진행
    - index, admin 페이지에 대해 USER,ADMIN 권한별 접근 테스트 추가

---
+++ src/test/java/com/tyranotyrano/springsecurityform/web/controller/AccountControllerTest.java
@@ -0,0 +1,62 @@
+package com.tyranotyrano.springsecurityform.web.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+class AccountControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void index_by_anonymous() throws Exception {
+		mockMvc.perform(get("/").with(anonymous()))
+			   .andDo(print())
+			   .andExpect(status().isOk());
+	}
+
+	@Test
+	void index_by_user() throws Exception {
+		mockMvc.perform(get("/").with(user("tyrano").roles("USER")))
+			   .andDo(print())
+			   .andExpect(status().isOk());
+	}
+
+	@Test
+	void index_by_admin() throws Exception {
+		mockMvc.perform(get("/").with(user("admin").roles("ADMIN")))
+			   .andDo(print())
+			   .andExpect(status().isOk());
+	}
+
+	@Test
+	void admin_by_user() throws Exception {
+		mockMvc.perform(get("/admin").with(user("tyrano").roles("USER")))
+			   .andDo(print())
+			   .andExpect(status().isForbidden()); // 403 : 일반 유저는 admin 페이지 접근 불가
+	}
+
+	@Test
+	void admin_by_admin() throws Exception {
+		mockMvc.perform(get("/admin").with(user("admin").roles("ADMIN")))
+			   .andDo(print())
+			   .andExpect(status().isOk());
+	}
+}
