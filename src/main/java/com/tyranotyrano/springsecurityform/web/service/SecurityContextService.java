57bf8c0df531a3bf4f59bba79e89e7d2f06b80c6
cyj0619 <cyj0619@midasit.com>
2021-10-19 09:44:55 +0900

    refactor(SecurityContextService.java): SecurityContextHolder 에서 Authentication 가져오도록 수정
    AccountContext 관련 로직 제거 및 SecurityContextHolder 에서 Authentication 가져오도록 수정한다.
    - ViewController : AccountContext 에 Account 저장하는 로직 제거
    - SecurityContextService : SecurityContextHolder 에서 Authentication 가져오도록 수정

--- src/main/java/com/tyranotyrano/springsecurityform/web/service/SecurityContextService.java
+++ src/main/java/com/tyranotyrano/springsecurityform/web/service/SecurityContextService.java
@@ -1,16 +1,18 @@
 package com.tyranotyrano.springsecurityform.web.service;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
-import com.tyranotyrano.springsecurityform.domain.account.Account;
-import com.tyranotyrano.springsecurityform.domain.account.AccountContext;
 
 @Service
 public class SecurityContextService {
 
 	public void confirmAuthentication() {
-		Account account = AccountContext.getAccount();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 		System.out.println("===================================");
-		System.out.println(account.getUsername());
+		System.out.println(authentication);
+		System.out.println(userDetails.getUsername());
 	}
 }
