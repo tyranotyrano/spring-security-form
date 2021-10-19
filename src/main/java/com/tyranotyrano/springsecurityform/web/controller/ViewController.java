99b65c1ec379608eb9d6963318c02b7b6d4dedab
tyranotyrano <tyrano09@gmail.com>
2021-10-19 09:44:55 +0900

    refactor(SecurityContextService.java): SecurityContextHolder 에서 Authentication 가져오도록 수정
    AccountContext 관련 로직 제거 및 SecurityContextHolder 에서 Authentication 가져오도록 수정한다.
    - ViewController : AccountContext 에 Account 저장하는 로직 제거
    - SecurityContextService : SecurityContextHolder 에서 Authentication 가져오도록 수정

--- src/main/java/com/tyranotyrano/springsecurityform/web/controller/ViewController.java
+++ src/main/java/com/tyranotyrano/springsecurityform/web/controller/ViewController.java
@@ -1,23 +1,18 @@
 package com.tyranotyrano.springsecurityform.web.controller;
 
-import java.security.Principal;
-
+import com.tyranotyrano.springsecurityform.web.service.SecurityContextService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.tyranotyrano.springsecurityform.domain.account.AccountContext;
-import com.tyranotyrano.springsecurityform.domain.repository.AccountRepository;
-import com.tyranotyrano.springsecurityform.web.service.SecurityContextService;
-
-import lombok.RequiredArgsConstructor;
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
 public class ViewController {
 
 	private final SecurityContextService securityContextService;
-	private final AccountRepository accountRepository;
 
 	@GetMapping("/")
 	public String index(Model model, Principal principal) {
@@ -45,7 +40,6 @@ public class ViewController {
 	public String dashboard(Model model, Principal principal) {
 		// Dashboard : 로그인한 사용자만 접근가능 -> Principal 필요
 		model.addAttribute("message", "dashboard : " + principal.getName());
-		AccountContext.setAccount(accountRepository.findByUsername(principal.getName()));
 		securityContextService.confirmAuthentication();
 		return "dashboard";
 	}
