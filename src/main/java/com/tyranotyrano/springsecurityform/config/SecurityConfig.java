12a8788ed7192aa93d3da9bebe806e6d7be75ff2
tyanotyrano <tyrano09@gmail.com>
2021-10-21 11:51:39 +0900

    config(SecurityConfig.java): RoleHierarchy 를 알고있는 Custom AccessDecisionManager 추가
    RoleHierarchy 를 알고있는 Custom AccessDecisionManager 를 추가한다.
    - SecurityConfig : RoleHierarchy(ADMIN > USER) 를 Custom AccessDecisionManager 에 설정

--- src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
+++ src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
@@ -1,12 +1,21 @@
 package com.tyranotyrano.springsecurityform.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +32,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.mvcMatchers("/", "/info", "/account/tmp/**").permitAll()
 			.mvcMatchers("/admin").hasRole("ADMIN")
 			.mvcMatchers("/user").hasRole("USER")
-			.anyRequest().authenticated();
+			.anyRequest().authenticated()
+			.accessDecisionManager(accessDecisionManager());
 		http.formLogin();
 		http.httpBasic();
+	}
+
+	/**
+	 * 사용자의 권한 상하관계 Hierarchy 를 설정한 AccessDecisionManager 를 만든다.
+	 *   - 권한 상하관계 : ADMIN > USER > ANONYMOUS
+	 * */
+	private AccessDecisionManager accessDecisionManager() {
+		RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+		roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+		DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+		handler.setRoleHierarchy(roleHierarchy);
+
+		WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+		webExpressionVoter.setExpressionHandler(handler);
+
+		List<AccessDecisionVoter<? extends Object>> voters = Arrays.asList(webExpressionVoter);
+		return new AffirmativeBased(voters);
 	}
 }
