7a6103b18dbca642fbd83d79dc8bbe35b0dc5d67
tyanotyrano <tyrano09@gmail.com>
2021-10-22 11:43:16 +0900

    config(SecurityConfig.java): 정적 자원 요청에 ignoring() 설정
    모든 정적 자원 요청에 대해 Spring Security Filter 가 적용되지 않도록 ignoring() 설정을 한다.
    - SecurityConfig : 정적 자원 요청에 대해 Spring Security Filter 가 적용되지 않도록 ignoring() 설정

--- src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
+++ src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.tyranotyrano.springsecurityform.config;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.AccessDecisionManager;
@@ -11,6 +12,7 @@ import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -25,6 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
 	}
 
 	@Override
