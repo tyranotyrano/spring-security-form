77ff814cc78a98f3f0c65d9de606cab51e69ab21
tyanotyrano <tyrano09@gmail.com>
2021-10-26 21:00:40 +0900

    config(SecurityConfig.java): accessDeniedHandler 설정
    인증/인가 예외관련 기능을 추가 하기위해 accessDeniedHandler 를 설정한다.
    - SecurityConfig : 인증/인가 예외 시, 로그를 남기는 accessDeniedHandler 추가

--- src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
+++ src/main/java/com/tyranotyrano/springsecurityform/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
@@ -52,7 +53,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.logout().logoutSuccessUrl("/");
 
 		http.exceptionHandling()
-			.accessDeniedPage("/access-denied");
+			.accessDeniedHandler((request, response, accessDeniedException) -> {
+				UserDetails principal = (UserDetails)SecurityContextHolder.getContext()
+																		  .getAuthentication()
+																		  .getPrincipal();
+				String username = principal.getUsername();
+				System.out.println(username + " is denied to access " + request.getRequestURI());
+				response.sendRedirect("/access-denied");
+			});
 
 		// http.sessionManagement()
 		// 	.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
